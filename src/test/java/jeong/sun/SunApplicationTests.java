d18c314619ea28483734bab4d4f8af02664bc1d2
byungsu.jeong <byungsu.jeong88@gmail.com>
2024-03-23 23:50:05 +0900

    init

---
+++ src/test/java/jeong/sun/SunApplicationTests.java
@@ -0,0 +1,13 @@
+package jeong.sun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
