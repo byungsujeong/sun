b5390e184848fa2356ca5f3adb54d046915dbf6a
byungsu.jeong <byungsu.jeong88@gmail.com>
2024-04-08 00:02:10 +0900

    jdbc and jpa

---
+++ src/main/java/jeong/sun/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package jeong.sun.controller;
+
+public class MemberForm {
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    private String Name;
+
+}
