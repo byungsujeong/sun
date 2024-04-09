db5ee7568f58bbc0e1e0a94d182b97d8e9263968
byungsu.jeong <byungsu.jeong88@gmail.com>
2024-04-09 15:44:33 +0900

    Spring data jpa

---
+++ src/main/java/jeong/sun/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package jeong.sun.repository;
+
+import jeong.sun.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
